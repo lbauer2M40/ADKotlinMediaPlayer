03e1abdd240a24a83372549b00cbdea2d5bd4ac5
lutzuca <lutzuca@gmx.de>
2021-05-28 13:52:02 +0200

    first commit

---
+++ app/src/main/java/at/ac/tgm/hit/medt8/lbauer/adkotlinproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package at.ac.tgm.hit.medt8.lbauer.adkotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/at/ac/tgm/hit/medt8/lbauer/adkotlinproject/MediaPlayer.kt
@@ -0,0 +1,11 @@
+package at.ac.tgm.hit.medt8.lbauer.adkotlinproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaPlayer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_player)
+    }
+}
