cd2c396a9f734a98c9cb2e6307d0560d36197730
lbauer2M40 <56685053+lbauer2M40@users.noreply.github.com>
2021-06-07 22:51:30 +0200

    erster Test mit einer ScrollView

--- app/src/main/java/at/ac/tgm/hit/medt8/lbauer/adkotlinproject/MainActivity.kt
+++ app/src/main/java/at/ac/tgm/hit/medt8/lbauer/adkotlinproject/MainActivity.kt
@@ -2,12 +2,20 @@ package at.ac.tgm.hit.medt8.lbauer.adkotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scroll : LinearLayout = findViewById(R.id.scrollLayout)
+        for(i in 1..100){
+            val media = Button(this.applicationContext)
+            scroll.addView(media)
+        }
     }
 
 }
